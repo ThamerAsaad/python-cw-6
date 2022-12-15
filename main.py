e351d0725aaff5918c677ae44f59d66ab36d69a3
ThamerAsaad <109876113+ThamerAsaad@users.noreply.github.com>
2022-12-15 22:03:43 +0300

    done

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+
 # write your code here
+person = {
+        'name': "thamer",
+        'age': 18,
+        'hobbies': ["drawing", "Gaming", "walking"]
+}
+
+print(person['name'])
+print(person['age'])
+
+person['age'] = 19
+person['country'] = "Saudi Arabia"
+
+
+print(person)
+print(len(person))
+
+person['hobbies'].append("tennis")
+
+def check_hobbies(person):
+    if len(person['hobbies']) > 3:
+        print("WOW YOU ARE AMAZING!")
+
+check_hobbies(person)    
